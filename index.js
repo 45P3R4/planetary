0f64f64b24f20d218190c8e4a4a2dffe60850799
ksamoilova <langleybaka@gmail.com>
2025-02-01 15:17:39 +0300

    simple planetary system

--- index.js
+++ index.js
@@ -6,26 +6,49 @@ const ctx = canvas.getContext("2d");
 ctx.fillStyle = "#FFFFFF"
 ctx.strokeStyle = "#FFFFFF"
 
-let xPos = innerWidth/2;
-let yPos = innerHeight/2;
-let epsilon = 0;
-let distance = 300;
+const centerPos = {x: innerWidth/2, y: innerHeight/2};
 
-function drawCircle(x, y, radius) {
-    ctx.beginPath();
-    ctx.arc(x, y, radius , 0, Math.PI * 2, true);
-    ctx.closePath();
-    ctx.stroke();
+class Body {
+    radius = 50;
+    epsilon = 0;
+    position = {x: 0, y: 0};
+
+    constructor(radius, position) {
+        this.radius = radius;
+        this.position.x = position.x;
+        this.position.y = position.y;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.position.x, this.position.y, this.radius , 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.stroke();
+    }
+
+    animate(center, distance) {
+        this.position.x = center.x + Math.sin(this.epsilon/distance)*distance;
+        this.position.y = center.y + Math.cos(this.epsilon/distance)*distance;
+        this.draw();
+        this.epsilon++;
+    }
 }
+
+let sun = new Body(50, centerPos);
+let planet = new Body(10, centerPos);
+let planet2 = new Body(5, centerPos);
+
+console.log(centerPos)
+
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    xPos += Math.sin(epsilon/distance);
-    yPos += Math.cos(epsilon/distance);
-    drawCircle(xPos - distance, yPos, 20);
-    drawCircle(innerWidth/2, innerHeight/2, 50);
+
+    sun.draw();
+    planet.animate(centerPos, 200);
+    planet2.animate(planet.position, 50);
+    
     window.requestAnimationFrame(animate);
-    epsilon++;
-  }
+}
   
   animate();
